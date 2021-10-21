317ff3319676ee3ebf52bddeb6bbe4d50c839e06
aimachieve <86616075+aimachieve@users.noreply.github.com>
2021-10-22 01:13:05 +0430

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require('cors');
 
 const users = require("./routes/api/users");
 
@@ -15,6 +16,13 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
